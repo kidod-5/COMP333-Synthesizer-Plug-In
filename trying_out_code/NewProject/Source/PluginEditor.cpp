6b80dcd062e4e05a32e06e82475bdea3421f497e
s4mmyb0i <138789917+s4mmyb0i@users.noreply.github.com>
2025-10-08 23:12:46 -0400

    JUCE basic plugin tutorial
    Went through a basic tutorial to create a small GUI and slider interface in JUCE

---
+++ trying_out_code/NewProject/Source/PluginEditor.cpp
@@ -0,0 +1,57 @@
+/*
+  ==============================================================================
+
+    This file contains the basic framework code for a JUCE plugin editor.
+
+  ==============================================================================
+*/
+
+#include "PluginProcessor.h"
+#include "PluginEditor.h"
+
+//==============================================================================
+NewProjectAudioProcessorEditor::NewProjectAudioProcessorEditor (NewProjectAudioProcessor& p)
+    : AudioProcessorEditor (&p), audioProcessor (p)
+{
+    // Make sure that before the constructor has finished, you've set the
+    // editor's size to whatever you need it to be.
+    setSize (200, 200);
+    // define parameters for slider object
+    midiVolume.setSliderStyle (juce::Slider::LinearBarVertical);
+    midiVolume.setRange (0.0, 127.0, 1.0);
+    midiVolume.setTextBoxStyle (juce::Slider::NoTextBox, false, 90, 0);
+    midiVolume.setPopupDisplayEnabled (true, false, this);
+    midiVolume.setTextValueSuffix (" Volume");
+    midiVolume.setValue (1.0);
+    // this function adds the slider to the editor
+    addAndMakeVisible (&midiVolume);
+    midiVolume.addListener(this);
+}
+
+NewProjectAudioProcessorEditor::~NewProjectAudioProcessorEditor()
+{
+}
+
+//==============================================================================
+void NewProjectAudioProcessorEditor::paint (juce::Graphics& g)
+{
+    // (Our component is opaque, so we must completely fill the background with a solid colour)
+    g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
+
+    g.setColour (juce::Colours::white);
+    g.setFont (juce::FontOptions (15.0f));
+    g.drawFittedText ("Midi Volume!", getLocalBounds(), juce::Justification::centred, 1);
+}
+
+void NewProjectAudioProcessorEditor::resized()
+{
+    // This is generally where you'll want to lay out the positions of any
+    // subcomponents in your editor..
+    
+    // sets the position and size of the slider with arguments (x, y, width, height)
+    midiVolume.setBounds (40, 30, 20, getHeight() - 60);
+}
+
+void NewProjectAudioProcessorEditor::::sliderValueChanged (juce::Slider* slider) {
+    audioProcessor.noteOnVel = midiVolume.getValue();
+}
